98c08e9541e088fc38d1387134be3241b27aa353
Arkadiy <goryunov.arkadiy@gmail.com>
2024-05-02 11:39:05 +0700

    Update fill db script, create db

--- scripts/fill_db.py
+++ scripts/fill_db.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 from database.container import person_service, movement_service
 from preparers.movement_preparer import MovementPreparer
 from preparers.person_preparer import PersonPreparer
@@ -11,7 +13,7 @@ movement_preparer = MovementPreparer()
 raw_questionnaires_data = questionnaire_parser.get_questionnaires_json()
 questionnaires_data = raw_questionnaires_data["questionnaires"]
 
-for questionnaire in questionnaires_data:
+for questionnaire in tqdm(questionnaires_data, total=len(questionnaires_data), desc="Заполнение БД"):
     person_data = person_preparer.get_person_data(questionnaire["name"], questionnaire["socialStatus"])
     person = person_service.create(person_data)
     questionnaire_date = questionnaire["dateMovements"]
