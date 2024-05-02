86dc66b66a28d3b6d73af94c453014b98fb172b4
Arkadiy <goryunov.arkadiy@gmail.com>
2024-05-02 12:06:45 +0700

    Add db to df script

--- questionnaires_analyzer.py
+++ questionnaires_analyzer.py
@@ -1,2 +1,2 @@
-class QuestionnairesАnalyzer:
+class QuestionnairesAnalyzer:
     pass
---
+++ scripts/db_to_df.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from database.container import movement_service
+
+
+def get_df_from_db() -> pd.DataFrame:
+    movements = movement_service.get_all()
+
+    data = []
+    for movement in movements:
+        data.append({
+            "Имя человека": movement.person.full_name,
+            "Социальный статус": movement.person.social_status,
+            "День недели": movement.week_day,
+            "Место отправления": movement.departure_place_type,
+            "Место прибытия": movement.arrival_place_type,
+            "Время отправления": movement.departure_time,
+            "lat": movement.departure_lat,
+            "lon": movement.departure_lon,
+        })
+
+    df = pd.DataFrame(data)
+    return df
